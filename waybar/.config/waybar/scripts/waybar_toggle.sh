254a0df6acb35fe001423fba5617229595788bdd
carlbodin <bodincarl@gmail.com>
2025-10-12 11:43:36 +0200

    Make waybar default safe

--- waybar/.config/waybar/scripts/waybar_toggle.sh
+++ waybar/.config/waybar/scripts/waybar_toggle.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Check if waybar is running
+# Toggle process.
 if pgrep -x "waybar" > /dev/null; then
-    # If running, kill the waybar process
     pkill -x "waybar"
 else
-    # If not running, start waybar
-    waybar &
+    # Check if pywal colors exist.
+    if [ -f "$HOME/.cache/wal/colors-waybar.css" ]; then 
+        waybar -s ~/.config/waybar/style.css &
+    else
+        waybar -s ~/.config/waybar/style_safe.css &
+    fi
 fi
 
 
