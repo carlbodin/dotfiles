4012635b25125e7a978ba779186edad88ce65d6b
carlbodin <bodincarl@gmail.com>
2025-11-25 20:14:14 +0100

    Cliphist: Add wrapper for toggle behavior

--- waybar/.config/waybar/scripts/cliphist_menu.sh
+++ waybar/.config/waybar/scripts/cliphist_menu.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-WOFI_ARGS="--dmenu --width 770 --allow-images --insensitive"
+# Check if wofi is running.
+if pgrep -x "wofi" > /dev/null; then
+    # Kill wofi if it's running.
+    pkill -x wofi
+else
 
-# Parse flags.
-case "$1" in
-    --wipe)
-        pkill -f "wl-paste.*cliphist"
-        rm -f ~/.cache/cliphist/db
-        wl-paste --type text --watch cliphist -max-items 15 store &
-        wl-paste --type image --watch cliphist -max-items 15 store &
-        cliphist wipe
-        exit 0
-        ;;
-    --delete)
-        cliphist list | wofi $WOFI_ARGS --prompt "Delete entry" | cliphist delete
-        exit 0
-        ;;
-esac
+    WOFI_ARGS="--dmenu --width 770 --allow-images --insensitive"
 
-# Normal clipboard selection.
-cliphist list | wofi $WOFI_ARGS --prompt "Select item to copy" | cliphist decode | wl-copy
+    # Parse flags.
+    case "$1" in
+        --wipe)
+            pkill -f "wl-paste.*cliphist"
+            rm -f ~/.cache/cliphist/db
+            wl-paste --type text --watch cliphist -max-items 15 store &
+            wl-paste --type image --watch cliphist -max-items 15 store &
+            cliphist wipe
+            exit 0
+            ;;
+        --delete)
+            cliphist list | wofi $WOFI_ARGS --prompt "Delete entry" | cliphist delete
+            exit 0
+            ;;
+    esac
+
+    # Normal clipboard selection.
+    cliphist list | wofi $WOFI_ARGS --prompt "Select item to copy" | cliphist decode | wl-copy
+fi
