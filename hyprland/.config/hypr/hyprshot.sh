1964cfb75443cbd0a0344c6a26e11bb54281b610
carlbodin <bodincarl@gmail.com>
2026-03-24 17:10:54 +0100

    Remove broken toggle logic inside hyprshot.sh

--- hyprland/.config/hypr/hyprshot.sh
+++ hyprland/.config/hypr/hyprshot.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Toggle if running.
-if pgrep -x "hyprshot" > /dev/null; then
-    pkill -x hyprshot
-    exit 0
-fi
-
 # Pass all arguments to hyprshot.
 hyprshot "$@" --output-folder "$HOME/Pictures/screenshots"
