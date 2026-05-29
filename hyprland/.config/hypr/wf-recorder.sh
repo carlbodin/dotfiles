f50921b609c0c6d04d0ada7d30153c8f273f8e7f
carlbodin <bodincarl@gmail.com>
2026-05-29 21:15:16 +0200

    Add icons to wf-recorder's notify-send

--- hyprland/.config/hypr/wf-recorder.sh
+++ hyprland/.config/hypr/wf-recorder.sh
@@ -3,17 +3,17 @@
 # Check if wf-recorder is running.
 if pgrep -x "wf-recorder" > /dev/null; then
     # If yes, kill gracefully.
-    pkill  -INT -x wf-recorder && notify-send "wf-recorder" "Screen recording stopped."
+    pkill  -INT -x wf-recorder && notify-send -i camera-video "wf-recorder" "Screen recording stopped."
 else
     # If not, start recording.
     if [[ "$1" == "--output" ]]; then
         mkdir -p "$HOME/Videos/screencasts"
-        notify-send "wf-recorder" "Fullscreen recording started, use the same bind to stop."
+        notify-send -i camera-video "wf-recorder" "Fullscreen recording started, use the same bind to stop."
         wf-recorder -o DP-1 -f "$HOME/Videos/screencasts/wf-recorder-$(date +%Y%m%d-%H%M%S).mkv"
     else
         GEOMETRY=$(slurp)
         mkdir -p "$HOME/Videos/screencasts"
-        notify-send "wf-recorder" "Selected area recording started, use the same bind to stop."
+        notify-send -i camera-video "wf-recorder" "Selected area recording started, use the same bind to stop."
         wf-recorder -g "$GEOMETRY" -f "$HOME/Videos/screencasts/wf-recorder-$(date +%Y%m%d-%H%M%S).mkv"
     fi
 fi
