0dc87a3c4595eb496a6590252d59b9cb8935cf87
carlbodin <bodincarl@gmail.com>
2026-05-29 20:48:12 +0200

    Improve wf-recorder script

--- hyprland/.config/hypr/wf-recorder.sh
+++ hyprland/.config/hypr/wf-recorder.sh
@@ -3,11 +3,12 @@
 # Check if wf-recorder is running.
 if pgrep -x "wf-recorder" > /dev/null; then
     # If yes, kill gracefully.
-    pkill  -INT -x wf-recorder && notify-send "Screen recording stopped."
+    pkill  -INT -x wf-recorder && notify-send "wf-recorder" "Screen recording stopped."
 else
     # If not, start recording.
+    GEOMETRY=$(slurp)
     mkdir -p "$HOME/Videos/screencasts"
-    wf-recorder -g "$(slurp)" -f "$HOME/Videos/screencasts/wf-recorder-$(date +%Y%m%d-%H%M%S).mkv"
-    notify-send "Screen recording started." "Use the same bind to stop."
+    notify-send "wf-recorder" "Screen recording started, use the same bind to stop."
+    wf-recorder -g "$GEOMETRY" -f "$HOME/Videos/screencasts/wf-recorder-$(date +%Y%m%d-%H%M%S).mkv"
 fi
 
