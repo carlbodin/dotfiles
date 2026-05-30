59a343cdbb2f9b278b6ea4884c23db5188f4ea09
carlbodin <bodincarl@gmail.com>
2026-05-30 13:29:09 +0200

    Update waybar config

--- waybar/.config/waybar/scripts/waybar_toggle.sh
+++ waybar/.config/waybar/scripts/waybar_toggle.sh
@@ -6,9 +6,9 @@ if pgrep -x "waybar" > /dev/null; then
 else
     # Check if pywal colors exist.
     if [ -f "$HOME/.cache/wal/colors-waybar.css" ]; then 
-        waybar -s ~/.config/waybar/style.css &
+        waybar -s ~/.config/waybar/style_wal.css &
     else
-        waybar -s ~/.config/waybar/style_safe.css &
+        waybar -s ~/.config/waybar/style.css &
     fi
 fi
 
