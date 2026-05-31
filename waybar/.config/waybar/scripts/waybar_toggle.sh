51f142b9d5915558098f2c86dc1ae1b80c44cab3
carlbodin <bodincarl@gmail.com>
2026-05-31 10:36:56 +0200

    Merge waybar style configs

--- waybar/.config/waybar/scripts/waybar_toggle.sh
+++ waybar/.config/waybar/scripts/waybar_toggle.sh
@@ -4,12 +4,7 @@
 if pgrep -x "waybar" > /dev/null; then
     pkill -x "waybar"
 else
-    # Check if pywal colors exist.
-    if [ -f "$HOME/.cache/wal/colors-waybar.css" ]; then 
-        waybar -s ~/.config/waybar/style_wal.css &
-    else
-        waybar -s ~/.config/waybar/style.css &
-    fi
+    waybar -s ~/.config/waybar/style.css &
 fi
 
 
